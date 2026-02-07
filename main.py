24699b2bf7508f7e43340dda24305f80e3a9ffc3
Khalid-Bensaber <202157500+Khalid-Bensaber@users.noreply.github.com>
2026-02-07 01:37:25 +0100

    chore: initial project setup

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from simulation-population!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from simpop.cli import main
+
+if __name__ == "__main__":
+    main()
